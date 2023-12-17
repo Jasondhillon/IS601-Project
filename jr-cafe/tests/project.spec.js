3a73fb006f73828e440a96c43e7bd5ebac0a9c8b
Jason Dhillon <jasondhillon419@icloud.com>
2023-12-16 20:11:13 -0500

    Test written for title

--- jr-cafe/tests/project.spec.js
+++ jr-cafe/tests/project.spec.js
@@ -3,10 +3,10 @@ const { test, expect } = require('@playwright/test');
 const homeURL = 'http://localhost:3000';
 // const aboutURL = 'http://localhost:3000/about.html';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Check Title - Homepage', async ({ page }) => {
+  await page.goto(homeURL);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle("JR Cafe - Home");
 });
 
