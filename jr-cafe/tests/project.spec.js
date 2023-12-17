7fc302a5176b068a4d36ee93edcc34f72832102b
Jason Dhillon <jasondhillon419@icloud.com>
2023-12-16 20:46:19 -0500

    Test written for Menu Section

--- jr-cafe/tests/project.spec.js
+++ jr-cafe/tests/project.spec.js
@@ -1,46 +1,46 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 const homeURL = 'http://localhost:3000';
-// const aboutURL = 'http://localhost:3000/about.html';
+const menuURL = 'http://localhost:3000/Menu';
 
 test('Check Title - Homepage', async ({ page }) => {
   await page.goto(homeURL);
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("JR Cafe - Home");
 });
 
 test('Check Navbar - Homepage', async ({ page }) => {
   await page.goto(homeURL);
 
-  // Expect a title "to contain" a substring.
   await expect(page.locator('nav').first()).toBeVisible();
 });
 
 test('Check Hero Section - Homepage', async ({ page }) => {
   await page.goto(homeURL);
 
-  // Expect a title "to contain" a substring.
   await expect(page.locator('section').nth(1)).toHaveId('herosection');
 });
 
 test('Check About Section - Homepage', async ({ page }) => {
   await page.goto(homeURL);
 
-  // Expect a title "to contain" a substring.
   await expect(page.locator('section').nth(2)).toHaveId('subsection');
 });
 
 test('Check Call to Action - Homepage', async ({ page }) => {
   await page.goto(homeURL);
 
-  // Expect a title "to contain" a substring.
   await expect(page.getByText('Sign up to receive coffee on the house!')).toBeVisible();
 });
 
 test('Check Email Input Box - Homepage', async ({ page }) => {
   await page.goto(homeURL);
 
-  // Expect a title "to contain" a substring.
   await expect(page.getByPlaceholder('Enter your email').first()).toBeVisible();
+});
+
+test('Check Menu Section - Menu page', async ({ page }) => {
+  await page.goto(menuURL);
+
+  await expect(page.locator('section').nth(1)).toHaveId('MenuSection');
 });
